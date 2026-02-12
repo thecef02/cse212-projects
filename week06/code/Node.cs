c25f6c855e1c8f73e6748e10f2b5b24bcc703ab2
thecef02 <thecef@hotmail.com>
2026-02-11 21:02:46 -0700

    02-11-26

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -21,6 +21,10 @@ public class Node
             else
                 Left.Insert(value);
         }
+        else if (value == Data)
+        {
+            return;
+        }
         else
         {
             // Insert to the right
@@ -34,12 +38,49 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (value == Data)
+        {
+            return true;
+        }
+
+        if (value < Data)
+        {
+            return Left is not null && Left.Contains(value);
+        }
+        else
+        {
+            return Right is not null && Right.Contains(value);
+        }        
+        // if (value < Data)
+        // {
+        //     // Check to the left
+        //     if (Left is null)
+        //     {
+        //         return false;
+        //     }
+
+        //     else
+        //         return Left.Contains(value);
+        // }
+        // if (value > Data)
+        // {
+        //     // Check to the right
+        //     if (Right is null)
+        //     {
+        //         return  false;
+        //     }
+
+        //     else
+        //         return Right.Contains(value);
+        // }
+        // return true;
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left is null ? 0 : Left.GetHeight();
+        int rightHeight = Right is null ? 0 : Right.GetHeight();
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
